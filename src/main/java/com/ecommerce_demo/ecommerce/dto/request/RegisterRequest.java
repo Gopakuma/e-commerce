b30d5e5832e5e58dec5c77a85369474d7f46f728
gopakuma <gopakumarg893@gmail.com>
2026-07-11 19:57:51 +0530

    User Entity , User Repo and DTO

---
+++ src/main/java/com/ecommerce_demo/ecommerce/dto/request/RegisterRequest.java
@@ -0,0 +1,30 @@
+package com.ecommerce_demo.ecommerce.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class RegisterRequest {
+    @NotBlank(message = "First Name is required")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+
+    @NotBlank(message = "User Name is required")
+    private String username;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must contain at least 8 characters")
+    private String password;
+}
