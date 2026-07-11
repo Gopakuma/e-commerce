8981d92ffc361a057a429dc900f23e7456097ca8
gopakuma <gopakumarg893@gmail.com>
2026-07-11 19:17:36 +0530

    Initial commit

---
+++ src/test/java/com/ecommerce_demo/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce_demo.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
