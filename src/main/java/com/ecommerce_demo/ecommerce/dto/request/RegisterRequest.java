b1950597af58eab61724fc3d7a9fe5bcb2c410d4
gopu <gopakumarg893@gmail.com>
2026-07-13 02:53:50 +0530

    Global Exception Handler first implementation

--- src/main/java/com/ecommerce_demo/ecommerce/dto/request/RegisterRequest.java
+++ src/main/java/com/ecommerce_demo/ecommerce/dto/request/RegisterRequest.java
@@ -11,20 +11,20 @@ import lombok.*;
 @Getter
 @Setter
 public class RegisterRequest {
-    @NotBlank
+    @NotBlank(message = "First name is required")
     private String firstName;
 
-    @NotBlank
+    @NotBlank(message = "Last name is required")
     private String lastName;
 
-    @NotBlank
+    @NotBlank(message = "User name is required")
     private String username;
 
-    @NotBlank
-    @Email
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid Email Format")
     private String email;
 
-    @NotBlank
+    @NotBlank(message = "Password is required")
     @Size(min = 8, message = "Password must contain at least 8 characters")
     private String password;
 }
---
+++ src/main/java/com/ecommerce_demo/ecommerce/exception/GlobalExceptionHandler.java
@@ -0,0 +1,87 @@
+package com.ecommerce_demo.ecommerce.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+   @ExceptionHandler(MethodArgumentNotValidException.class)
+   public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(
+           MethodArgumentNotValidException ex,
+           HttpServletRequest request) {
+
+      String errorMessage = ex.getBindingResult()
+              .getFieldErrors()
+              .stream()
+              .map(error -> error.getField() + ": " + error.getDefaultMessage())
+              .collect(Collectors.joining(", "));
+
+      ErrorResponse errorResponse = ErrorResponse.builder()
+              .timestamp(LocalDateTime.now())
+              .status(HttpStatus.BAD_REQUEST.value())
+              .error(HttpStatus.BAD_REQUEST.getReasonPhrase())
+              .message(errorMessage)
+              .path(request.getRequestURI())
+              .build();
+
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+              .body(errorResponse);
+   }
+
+   @ExceptionHandler(UserAlreadyExistsException.class)
+    public ResponseEntity<ErrorResponse> handleUserAlreadyExistsException(
+            UserAlreadyExistsException ex,
+            HttpServletRequest request) {
+
+       ErrorResponse errorResponse = ErrorResponse.builder()
+               .timestamp(LocalDateTime.now())
+               .status(HttpStatus.CONFLICT.value())
+               .error(HttpStatus.CONFLICT.getReasonPhrase())
+               .message(ex.getMessage())
+               .path(request.getRequestURI())
+               .build();
+
+       return ResponseEntity.status(HttpStatus.CONFLICT)
+               .body(errorResponse);
+   }
+
+   @ExceptionHandler(UserRegistrationFailedException.class)
+    public ResponseEntity<ErrorResponse> handleUserRegistrationFailedException(
+            UserRegistrationFailedException ex,
+            HttpServletRequest request) {
+
+       ErrorResponse errorResponse = ErrorResponse.builder()
+               .timestamp(LocalDateTime.now())
+               .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+               .error(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase())
+               .message(ex.getMessage())
+               .path(request.getRequestURI())
+               .build();
+
+       return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+               .body(errorResponse);
+   }
+
+   @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(
+            HttpServletRequest request) {
+
+       ErrorResponse errorResponse = ErrorResponse.builder()
+               .timestamp(LocalDateTime.now())
+               .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+               .error(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase())
+               .message("Something went wrong")
+               .path(request.getRequestURI())
+               .build();
+
+       return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+               .body(errorResponse);
+   }
+}
